7e34b430b9ffff1ef46808e1bf33ddf601ac6c49
Penny-Cho <tinysalmon0@gmail.com>
2022-04-17 15:40:32 +0900

    added header and signout

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -10,14 +10,14 @@ export default ({ url, method, body, onSuccess }) => {
       const response = await axios[method](url, body);
 
       if (onSuccess) {
-        onSuccess(response.data);
+        onSuccess(response?.data);
       }
 
       return response.data;
     } catch (err) {
       setErrors(
         <div className="alert alert-danger mt-4">
-          {err.response.data.errors.map((err) => (
+          {err.response?.data.errors.map((err) => (
             <p className="text-error" key={err.field}>
               {err.message}
             </p>
